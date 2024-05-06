feb591dcbefe6c570a80e16b41954e74a3cfbca9
Marina745 <158029971+Marina745@users.noreply.github.com>
2024-05-06 11:19:29 +0300

    44

---
+++ market.js
@@ -0,0 +1,12 @@
+//перевірка підключеного файлу скриптів market.js
+console.log('Перевірка підключеного файлу скриптів market.js')
+ //Отримання елементу з індифікатором items
+ let itemsDiv = document.getElementById("items");
+
+ //Перевірка існування знайденого блоку
+ if (itemsdiv) {
+  //Вивід знайденого елементу
+ console.log(itemsDiv)
+ } else{
+    console.log('Блок товарів')
+ }
